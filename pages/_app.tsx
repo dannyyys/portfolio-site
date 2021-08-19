c7f44dd67341312df6def2cb493c32333dbcf001
dannyyys <dannyyys@users.noreply.github.com>
2021-08-19 22:23:45 +0800

    refactor: :recycle: Update page title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -19,7 +19,7 @@ export default function App(props: AppProps) {
     <>
       <JssProvider generateId={createGenerateId({ minify: true })}>
         <Head>
-          <title>Mantine next example</title>
+          <title>YS portfolio</title>
           <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
         </Head>
 
